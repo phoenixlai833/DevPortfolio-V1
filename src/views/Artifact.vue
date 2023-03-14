44b03a7cb9365137407beb72d47e70ff644df514
Phoenix Lai <plai40@my.bcit.ca>
2023-03-13 23:11:25 -0700

    fix routing

--- src/views/Artifact.vue
+++ src/views/Artifact.vue
@@ -1,25 +1,26 @@
 <script setup>
 import ArtifactRight from '../components/ArtifactRight.vue';
+import { useRoute } from "vue-router";
 </script>
 
 <template>
     <div id="artifact">
         <div class="leftside">
             <!-- <div class="back_arrow_container" @click="$router.go(-1)"><img class="back_arrow"
-                                                                    src="/assets/icons/arrowLeft.svg" /></div> -->
+                                                                                                                        src="/assets/icons/arrowLeft.svg" /></div> -->
             <a href="#Projects">
                 <div class="back_arrow_container"><img class="back_arrow" src="/assets/icons/arrowLeft.svg" /></div>
             </a>
             <div class="title_container">
-                <span v-if="this.artifact == 'LocalToMe'">LocalToMe</span>
-                <span v-if="this.artifact == 'EvolveSite'">Evolve Site</span>
-                <span v-if="this.artifact == 'DowcoConsultants'">Dowco Consultants</span>
-                <span v-if="this.artifact == 'InnotaTech'">Innota Tech</span>
+                <span v-if="artifactId == 'LocalToMe'">LocalToMe</span>
+                <span v-if="artifactId == 'EvolveSite'">Evolve Site</span>
+                <span v-if="artifactId == 'DowcoConsultants'">Dowco Consultants</span>
+                <span v-if="artifactId == 'InnotaTech'">Innota Tech</span>
                 <!-- <div class="up_arrow_container"><img class="up_arrow" src="/assets/icons/arrowUp.svg" /></div> -->
             </div>
         </div>
         <div class="rightside">
-            <div v-if="this.artifact == 'LocalToMe'">
+            <div v-if="artifactId == 'LocalToMe'">
                 <ArtifactRight heroImage="/assets/images/f2-LocalToMe.png"
                     githubLink="https://github.com/phoenixlai833/LocalToMe" siteLink="https://localtome.vercel.app/"
                     context="LocalToMe is a web app that locates and provides low-income families/individuals with free or accessible food resources near their area within their budgets. Users can search for food banks on the map page, easily find out if they qualify, get directions and more. To build community, signed-in users are able to post news and events that relate to this topic. They can also customize their profiles and view saved food banks or events in the favourites page."
@@ -29,7 +30,7 @@ import ArtifactRight from '../components/ArtifactRight.vue';
                     prj1="/assets/images/f1-Evolve.png" prj2="/assets/images/f3-Dowco.png"
                     prj3="/assets/images/f1-Evolve.png" />
             </div>
-            <div v-if="this.artifact == 'EvolveSite'">
+            <div v-if="artifactId == 'EvolveSite'">
                 <ArtifactRight heroImage="/assets/images/f1-Evolve.png" siteLink="https://evolve-website-a0510.web.app/"
                     context="Helped build out the new design for Evolve Branding’s company site, an extension for their EvolveX app ecosystem. Learned and coded in Flutter to allow the web app to be easily scaled between desktop and mobile. Designed new pages as needed and implemented them on approval. Attended regular meetings with the founder and corresponded with fellow developers in the team throughout the process."
                     :technologies="['Flutter', 'Firebase', 'Android Studios']" demoVideo="" challengeBig=""
@@ -37,7 +38,7 @@ import ArtifactRight from '../components/ArtifactRight.vue';
                     takeawaysSmall="" niceToHaves="" prj1="/assets/images/f2-LocalToMe.png"
                     prj2="/assets/images/f3-Dowco.png" prj3="/assets/images/f1-Evolve.png" />
             </div>
-            <div v-if="this.artifact == 'DowcoConsultants'">
+            <div v-if="artifactId == 'DowcoConsultants'">
                 <ArtifactRight heroImage="/assets/images/f3-Dowco.png" siteLink="https://www.dowco.com/"
                     context="Redesigned and developed a new homepage and navigation for Dowco Construction Consultants, to better showcase the company and fix any issues with the current version. Communicated directly with the client from ideation to development to understand their vision and pivot as necessary. Had to isolate the new homepage and build new navigation upon existing sites files without compromising the rest of the pages."
                     :technologies="['HTML', 'SCSS', 'jQuery']" demoVideo="" challengeBig="" challengeSmall="" solutionBig=""
@@ -45,7 +46,7 @@ import ArtifactRight from '../components/ArtifactRight.vue';
                     prj1="/assets/images/f2-LocalToMe.png" prj2="/assets/images/f1-Evolve.png"
                     prj3="/assets/images/f1-Evolve.png" />
             </div>
-            <div v-if="this.artifact == 'InnotaTech'">
+            <div v-if="artifactId == 'InnotaTech'">
                 <ArtifactRight heroImage="/assets/images/f4-InnotaTech.png" inProgress="true"
                     context="Helped build out the new design for Evolve Branding’s company site, an extension for their EvolveX app ecosystem. Learned and coded in Flutter to allow the web app to be easily scaled between desktop and mobile. Designed new pages as needed and implemented them on approval. Attended regular meetings with the founder and corresponded with fellow developers in the team throughout the process."
                     :technologies="['React', 'MVC', 'Mongodb', 'Docker']" demoVideo="" challengeBig="" challengeSmall=""
@@ -61,13 +62,10 @@ import ArtifactRight from '../components/ArtifactRight.vue';
 <script>
 export default {
     name: "Artifact",
-    data() {
-        return {
-            artifact: null
+    computed: {
+        artifactId() {
+            return this.$route.params.id;
         }
-    },
-    mounted() {
-        this.artifact = this.$route.params.id
     }
 };
 </script>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1>PAGE NOT FOUND</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "NotFound"
+};
+</script>
+
+<style scoped>
+</style>
